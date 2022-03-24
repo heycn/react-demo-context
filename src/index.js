08f24fc3b27f3badbc9c26f35c13e8c9c01ee958
heycn <x1chennan@qq.com>
2022-03-24 13:20:36 +0800

    请看源代码

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import ReactDOM from 'react-dom'
-import './style.css'
+
+const C = createContext(null)
 
 const App = () => {
+  const [n, setN] = useState(0)
   return (
-    <div className='App'>hi</div>
+    <C.Provider value={{ n, setN }}>
+      <div className='App'>
+        <Parent />
+      </div>
+    </C.Provider>
+  )
+}
+
+const Parent = () => {
+  return (
+    <div>
+      我是爸爸
+      <Child />
+    </div>
+  )
+}
+
+const Child = () => {
+  const { n, setN } = useContext(C)
+  const onClick = () => {
+    setN(i => i + 1)
+  }
+  return (
+    <div>
+      我是儿子 我得到的 n 是：{n}
+      <button onClick={onClick}>n+1</button>
+    </div>
   )
 }
 
